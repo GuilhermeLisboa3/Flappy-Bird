9707826f2893650df2b16986144ade4d70778c66
Guilherme Gonçalves Lisboa <guime0162@gmail.com>
2022-08-15 14:51:24 -0300

    Criando função para gerar barreiras e animar elas

--- js/flappy.js
+++ js/flappy.js
@@ -39,5 +39,38 @@ function ParDeBarreiras(altura, abertura, x) {
   this.sortearAbertura();
   this.setX(x);
 }
-const b = new ParDeBarreiras(700, 200, 400);
-document.querySelector("[wm-flappy]").appendChild(b.elemento);
+
+
+function Barreiras(altura, largura, abertura, espaco, notificarPonto){  
+    this.pares = [
+        new ParDeBarreiras(altura, abertura, largura),
+        new ParDeBarreiras(altura, abertura, largura + espaco),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 2),
+        new ParDeBarreiras(altura, abertura, largura + espaco * 3),
+    ]
+
+    const deslocamento = 3
+    this.animar = ()=>{
+        this.pares.forEach(par =>{
+            par.setX(par.getX() - deslocamento)
+
+            if(par.getX() < par.getLargura()){
+                par.setX(par.getX() + espaco * this.pares.length)
+                par.sortearAbertura()
+            }
+
+            const meio = largura / 2
+            const cruzouOMeio = par.getX() + deslocamento >= meio && par.getX() < meio
+            if(cruzouOMeio) notificarPonto()
+        })
+    }
+}
+
+const barreiras = new Barreiras(700,1200,200,400)
+const areaDoJogo = document.querySelector('[wm-flappy]')
+
+barreiras.pares.forEach(par => areaDoJogo.appendChild(par.elemento))
+
+setInterval(()=>{
+    barreiras.animar()
+},20)
